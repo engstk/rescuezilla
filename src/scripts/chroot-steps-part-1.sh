dc38c387845a2668a94b539358e7ea418c4e8e6e
engstk <eng.stk@sapo.pt>
2025-03-31 17:03:24 +0100

    Add zip utils
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- src/scripts/chroot-steps-part-1.sh
+++ src/scripts/chroot-steps-part-1.sh
@@ -378,6 +378,8 @@ common_pkgs=("discover"
              "sg3-utils"
              "vorta"
              "borgbackup"
+             "zip"
+             "unzip"
 )
 
 # Install openssh-server only if the IS_INTEGRATION_TEST variable is enable
